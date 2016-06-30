780f8b69770c802a4272c2068f256bf38afbe0b5
Marc Brändli <m1braend@hsr.ch>
2016-06-30 21:18:18 +0200

    sh update

--- assemble_send_print.sh
+++ assemble_send_print.sh
@@ -3,14 +3,15 @@
 #BEARBEITEN und Speichern via imageMagick und usbmount
 mogrify -resize 968x648 ~/photobooth/photobooth_images/*.jpg
 montage ~/photobooth/photobooth_images/*.jpg -tile 2x2 -geometry +10+10 ~/photobooth/temp_montage.jpg
-suffix=$(date +%d%m%Y_%H%M)
-montage ~/photobooth/temp_montage.jpg ~/photobooth/footer.jpg -tile 2x1 -geometry +5+5 /media/usb0/photobooth_archive/photobox_${suffix}.jpg
+datum=$(date +%d%m%Y_%H%M)
+mailadresse=$(marc@braendli.ch)
+montage ~/photobooth/temp_montage.jpg ~/photobooth/footer.jpg -tile 2x1 -geometry +5+5 /media/usb0/photobooth_archive/photobox_${datum}.jpg
 
 #VERSENDEN via ssmpt/mailutils/mpack
-mail < ~/photobooth/mail_message marc@braendli.ch -s "Photobox" -A "/media/usb0/photobooth_archive/photobox_${suffix}.jpg" 
+mail < ~/photobooth/mail_message ${mailadresse} -s "Photobox" -A "/media/usb0/photobooth_archive/photobox_${suffix}.jpg" 
 
 #DRUCKEN via CUPS
-#lp -d NPD DS_RX1 /media/usb0/photobooth_archive/photobox_${suffix}.jpg
+#lp -d NPD DS_RX1 /media/usb0/photobooth_archive/photobox_${datum}.jpg
 
 #AUFRAEUMEN
 #rm /home/pi/photobooth_images/*.jpg
