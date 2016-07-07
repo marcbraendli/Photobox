95aac2700434297d79c085d75259b02cbad908ad
Marc Brändli <m1braend@hsr.ch>
2016-07-07 09:58:07 +0200

    shell-file extended

--- kivy-src/main.py
+++ kivy-src/main.py
@@ -1,19 +1,24 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.vkeyboard import VKeyboard
+from kivy.clock import Clock
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty, ListProperty
 
 
-class LoginScreen(BoxLayout):
-    pass
-
-
+class LoginScreen(Widget):
+ pass
+    
 class TakePhoto(Widget):
-    pass
+ pass
 
  
 class MyApp(App):
-    def build(self):
-        return LoginScreen()
+        def build(self):
+            return LoginScreen()
 
     
 if __name__ == '__main__':
