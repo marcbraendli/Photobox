cbf48316515ba98f549225eebd8e8dfa7e4381ce
Marc <marc_braendli@hotmail.com>
2016-09-10 20:38:24 +0200

    Photo assembly revised 2

--- kivy-src/main.py
+++ kivy-src/main.py
@@ -108,13 +108,17 @@ class Countdown(AnchorLayout):
         #self.send_mail(mailadress)
         self.clean_up()
         
-    def assembly(self, *kwargs):
-       print "1/3"
-       os.system("mogrify -resize 968x648 ~/workspace/capture_images/*.jpg")
-       print "2/3"
-       os.system("montage ~/workspace/capture_images/*.jpg -tile 2x2 -geometry +10+10 ~/workspace/temp_montage.jpg")
-       print "3/3"
-       os.system("montage ~/workspace/temp_montage.jpg ~/workspace/footer.jpg -tile 2x1 -geometry +5+5 ~/workspace/photobox_%s.jpg" %TIMESTAMP)
+    def assembly(self, *kwargs):  
+        #fkt inhalt getestet und kalibriert
+        print "1/3-auskommentiert"
+        os.system("mogrify -resize 856x570 ~/workspace/capture_images/*.jpg")
+        print "2/3"
+        os.system("montage ~/workspace/capture_images/*.jpg -tile 2x2 -geometry +10+10 ~/workspace/temp_montage.jpg")
+        os.system("montage ~/workspace/temp_montage.jpg -geometry +4+23 ~/workspace/temp_montage2.jpg")
+        print "3/3"
+        os.system("montage ~/workspace/temp_montage2.jpg ~/workspace/footer_3.jpg -tile 2x1 -geometry +2+0 ~/workspace/temp_montage3.jpg")
+        os.system("montage ~/workspace/temp_montage3.jpg -geometry +0+20 ~/workspace/photobox_%s.jpg" %TIMESTAMP)
+        print "finished"
     
     def send_mail(self, mailadress, *kwargs):
        os.system("mail < ~/workspace/mail_message %s -s "Photobox" -A "~/workspace/photobox_%s.jpg" %(mailadress, TIMESTAMP)) 
